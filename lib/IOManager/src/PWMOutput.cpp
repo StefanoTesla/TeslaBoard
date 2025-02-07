d20bac9fc1e4f0c1a4d2ad846ab9fddcb571f3aa
StefanoTesla <stefano.martini.fi@gmail.com>
2025-02-07 17:11:25 +0100

    ALPHA DON'T USE IT TENSTING OBJECT LIBRARY

--- lib/IOManager/src/PWMOutput.cpp
+++ lib/IOManager/src/PWMOutput.cpp
@@ -11,9 +11,9 @@ void PWMOutput::setup(int _pin, int _channel){
     ledcAttachPin(pin,channel);
 }
 
-int PWMOutput::write(int _valore) {
-    if(_valore > 0 && _valore < 4096){
-        ledcWrite(channel,_valore);
+int PWMOutput::write(int _value) {
+    if(_value > 0 && _value < 4096){
+        ledcWrite(channel,_value);
         return 1;
     } else {
         return 0;
