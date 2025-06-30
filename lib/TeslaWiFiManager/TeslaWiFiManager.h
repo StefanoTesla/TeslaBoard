2f175eda6a7d4726117b68057fcf818c7d942db4
StefanoTesla <92517402+StefanoTesla@users.noreply.github.com>
2025-06-30 14:21:15 +0200

    lock asyncwebserverversion to 3.7.7 for captive problems

---
+++ lib/TeslaWiFiManager/TeslaWiFiManager.h
@@ -0,0 +1,57 @@
+#ifndef TESLA_WIFI_MNG_H
+#define TESLA_WIFI_MNG_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncJson.h>
+#include <ArduinoJson.h>
+#include "LittleFS.h"
+
+
+class IOBase {
+protected:
+    int pin;
+    char Name[21];
+    char Description[21];
+    int type;
+    unsigned int min;
+    unsigned int max;
+
+public:
+    IOBase() {}
+    virtual void setup(IOConfigBase* config){}
+    virtual int write(int _value) = 0;
+    virtual int readPin() = 0;
+    virtual int status();
+    virtual int getType(){
+        return 0;
+    }
+    virtual unsigned int getMin(){
+        return min;
+    }
+    virtual unsigned int getMax(){
+        return max;
+    }
+    virtual int getPinNumber(){
+        return pin;
+    }
+    virtual void setName(const char* _name){
+       strncpy(Name, _name, sizeof(Name) - 1);
+       Name[sizeof(Name) - 1] = '\0';
+    }
+    virtual void setDescription(const char* _description){
+       strncpy(Description, _description, sizeof(Description) - 1);
+       Description[sizeof(Description) - 1] = '\0';
+    }
+
+    virtual const char* getName(){
+        return Name;
+    }
+    virtual const char* getDescription(){
+        return Description;
+    }
+    virtual ~IOBase() {}
+};
+
+#endif
