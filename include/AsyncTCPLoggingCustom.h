874b71730ff29240b356d12779783fa4e11b07ad
StefanoTesla <92517402+StefanoTesla@users.noreply.github.com>
2026-09-24 18:16:58 +0200

    muting asyncwebserver log

---
+++ include/AsyncTCPLoggingCustom.h
@@ -0,0 +1,6 @@
+#pragma once
+#define async_tcp_log_e(format, ...) do {} while (0)
+#define async_tcp_log_w(format, ...) do {} while (0)
+#define async_tcp_log_i(format, ...) do {} while (0)
+#define async_tcp_log_d(format, ...) do {} while (0)
+#define async_tcp_log_v(format, ...) do {} while (0)
---
+++ include/AsyncWebServerLoggingCustom.h
@@ -0,0 +1,6 @@
+#pragma once
+#define async_ws_log_e(format, ...) do {} while (0)
+#define async_ws_log_w(format, ...) do {} while (0)
+#define async_ws_log_i(format, ...) do {} while (0)
+#define async_ws_log_d(format, ...) do {} while (0)
+#define async_ws_log_v(format, ...) do {} while (0)
